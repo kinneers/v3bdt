f0cfbbdf8a79ae5f0f08a1fc280cd1daad2cba7e
Billy Sterling <44212511+BillySterling@users.noreply.github.com>
2019-04-11 22:08:16 -0400

    database model updates, removed scripts and seeds; added teacherController

--- models/teacher.js
+++ models/teacher.js
@@ -7,7 +7,10 @@ const TeacherSchema = new Schema({
   lastName: { type: String, required: true },
   authLevel: { type: Number, enum: [1, 2, 3, 4, 5], required: true },
   classes: [{ type: String, required: false}],
-  students: [{ type: Schema.Types.ObjectId, ref: "Student", required: false}]
+  students: [{ type: Schema.Types.ObjectId, ref: "Student", required: false}],
+  status: {type: Boolean, required: true, default: true},
+  createdAt: { type: Date, default: Date.now() },
+  updatedAt: { type: Date, default: Date.now() }
 });
 
 const Teacher = mongoose.model("Teacher", TeacherSchema);
