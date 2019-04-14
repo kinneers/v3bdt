a697bf1310c83c130a7b5bf4cd5f4aa8ff4dfea2
Sarah Kinneer <kinneers@gmail.com>
2019-04-14 14:52:08 -0400

    Updated some logic on rating form, still working on handling multiple changes on single row before data saved

--- client/src/components/RatingForm/index.js
+++ client/src/components/RatingForm/index.js
@@ -36,20 +36,50 @@ class RatingForm extends Component {
 
     // Handles updating component state when the user types into the input field
     handleInputChange = event => {
+        //Gathers the current state of the new Data
         const prevData = this.state.newData;
+        //Gets the name and value of the target that changed
         const { name, value } = event.target;
+        //Sets newestData to an object with a key of the name (behaviorID) and value of the value (rating) of the current target
         const newestData = {[name]: value}
-        this.setState({
-            newData: [...prevData, newestData]
-        });
+        //Gets the key of the newest data value
+        const arrayDataKey = Object.keys(newestData);
+        const newestDataKey = arrayDataKey[0];
+        //Gets the value of the newest data value
+        const newestKeyValue = newestData[Object.keys(newestData)];
+        console.log('Newest Data: ', newestData);
+        console.log('The key of Newest Data: ', newestDataKey);
+        console.log('The value of Newest Data: ', newestKeyValue);
+        console.log('Previous Data: ', prevData);
+        //Initializes an array to store keys
+        let prevKeysArray =[];
+        for (let i = 0; i < prevData.length; i++) {
+            //Gathers the value of the object's key at each iteration
+            let keyInArray = Object.keys(prevData[i]);
+            //Pushes the value of the object's key to the previous keys array
+            prevKeysArray.push(...keyInArray);
+            //Checks if the newest data's key is already in the prevKeysArray
+            if (prevKeysArray.includes(newestDataKey)) {
+                console.log('This key is already in the array')
+                //Get the index of this in the array
+                //Create logic to update the existing key in the state
+            } else {
+                this.setState({
+                    newData: [...prevData, newestData]
+                });
+            }
+        }
     };
 
-//   // When the form is submitted, use the API.saveBook method to save the book data
-//   // Then reload books from the database
+//   // When the form is submitted, use the API.saveData method to save the bx data
+//   // Then reset form
     saveData = event => {
         event.preventDefault();
         console.log('Button Clicked!');
         console.log(this.state);
+
+        //If the value is '', do nothing
+
 //     if (this.state.value) {
 //       API.saveData({
 //         behaviorID: this.state.behaviorID,
@@ -58,6 +88,7 @@ class RatingForm extends Component {
 //         .then(res => this.loadBehaviors())
 //         .catch(err => console.log(err));
 //     }
+        this.setState({ newData: [] })
     };
 
     render() {
@@ -100,7 +131,7 @@ class RatingForm extends Component {
                                                         <span>Not Met</span>
                                                     </label>
                                                     <label>
-                                                        <input className="with-gap" id="N/A" value={this.isNull} name={behaviorInfo.behaviorID} type="radio"/>
+                                                        <input className="with-gap" id="N/A" value={''} name={behaviorInfo.behaviorID} type="radio" defaultChecked/>
                                                         <span>N/A</span>
                                                     </label>
                                                 </form>
