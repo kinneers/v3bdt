ad663c5638daf8eda06bc5f474502a4b954f90ac
Jodi Bell <jodi@jodibell.com>
2019-04-15 18:37:11 -0400

    seed working woohoo

---
+++ controllers/studentController.js
@@ -0,0 +1,40 @@
+const db = require("../models");
+
+// Defining methods for the StudentsController
+module.exports = {
+    //ADD:
+    //findAllBehaviorsRefStudent- needs to get each behavior related to that Student in form of array of objects with AT LEAST: {behaviorID, name, behavior, studentID}
+
+  findAll: function(req, res) {
+    db.Student
+      .find(req.query)
+      .sort({ date: -1 })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  findById: function(req, res) {
+    db.Student
+      .findById(req.params.id)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  create: function(req, res) {
+    db.Student
+      .create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  update: function(req, res) {
+    db.Student
+      .findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  remove: function(req, res) {
+    db.Student
+      .findById({ _id: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  }
+};
