3aacdeed118ac265332dc5c9efe31ddc1d60b146
Sarah Kinneer <kinneers@gmail.com>
2019-04-20 13:39:30 -0400

    lifted state to Teacher.js

--- client/src/components/SideNav/index.js
+++ client/src/components/SideNav/index.js
@@ -1,63 +1,28 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './style.css';
-import M from "materialize-css";
-import API from './../../utils/API';
-
-class SideNav extends Component {
-    state = {
-        behaviorInfo: {},
-        teacherID: '5cb8e2bd4c3e69054020ac33',
-        chosenBxId: '5cb8e2bf4c3e69054020ae5a'
-    }
-
-    componentDidMount() {
-        // Auto initialize all the things!
-        M.AutoInit();
-        let id = this.state.teacherID;
-        this.loadBehaviors(id);
-    }
-
-    // Loads all behaviors and sets them to this.state.behaviorInfo
-    loadBehaviors = (id) => {
-        API.getBehaviors(id)
-            .then(res => 
-                {                    
-                    this.setState({ behaviorInfo: res.data });
-                    M.AutoInit();
-                    //This needs to happen in a click event later
-                    this.getChartData();
-                })
-            .catch(err => console.log(err));
-    };
-
-    componentDidUpdate() {
-        M.AutoInit();
-    }
-
-    render() {
-        return(
-            <div>
-                <ul id="slide-out" className="sidenav sidenav-fixed">
-                    <li className="top">Welcome!</li>
-                        {(this.state.behaviorInfo.behaviors) ? (
-                            <ul className="collapsible" >
-                                {this.state.behaviorInfo.behaviors.map(behavior => (
-                                    <li key={behavior._id}>
-                                        <div className="collapsible-header"><i className="material-icons">assignment</i>{behavior.studentName}
-                                            <div><i className="material-icons">expand_more</i></div>
-                                        </div>
-                                        <div className="collapsible-body"><span><a href="#!" value={behavior._id}>{behavior.behaviorName}</a></span></div>
-                                    </li>                                    
-                                ))}
-                            </ul>
-                    ) : 
-                    (<p>No Students</p>)}
-                    
-                </ul>
-                <a href="#!" data-target="slide-out" className="sidenav-trigger"><i className="material-icons">menu</i></a>
-            </div>
-        );
-    }
-};
+    
+function SideNav(props) {
+    return(
+        <div>
+            <ul id="slide-out" className="sidenav sidenav-fixed">
+                <li className="top">Welcome!</li>
+                    {(props.behaviorInfo.behaviors) ? (
+                        <ul className="collapsible" >
+                            {props.behaviorInfo.behaviors.map(behavior => (
+                                <li key={behavior._id}>
+                                    <div className="collapsible-header"><i className="material-icons">assignment</i>{behavior.studentName}
+                                        <div><i className="material-icons">expand_more</i></div>
+                                    </div>
+                                    <div className="collapsible-body"><span><a href="#!" value={behavior._id}>{behavior.behaviorName}</a></span></div>
+                                </li>                                    
+                            ))}
+                        </ul>
+                ) : 
+                (<p>No Students</p>)}
+            </ul>
+            <a href="#!" data-target="slide-out" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+        </div>
+    );
+}
 
 export default SideNav;
--- client/src/pages/Teacher/Teacher.js
+++ client/src/pages/Teacher/Teacher.js
@@ -1,32 +1,122 @@
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
+//import { Route, Switch } from 'react-router-dom';
 import RatingForm from './../../components/RatingForm';
 import TopNav from './../../components/TopNav';
 import SideNav from './../../components/SideNav';
 import BxChart from './../../components/BxChart';
 import './style.css';
+import M from "materialize-css";
+import API from './../../utils/API';
 
 class Teacher extends Component {
     //Initial State- we definitely need to set the current user, but not sure how to do it with Cognito
     state = {
-        teacher: '',
+        behaviorInfo: {},
+        teacherID: '5cb8e2bd4c3e69054020ac33',
+        chosenBxId: '5cb8e2bf4c3e69054020ae5a',
+        newData: {},
+        radioButtons: {}
+    };
+
+    componentDidMount() {
+        // Auto initialize all the things!
+        M.AutoInit();
+        let id = this.state.teacherID;
+        this.loadBehaviors(id);
+    }
+
+    // Loads all behaviors and sets them to this.state.behaviorInfo
+    loadBehaviors = (id) => {
+        API.getBehaviors(id)
+            .then(res => 
+                {                    
+                    this.setState({ behaviorInfo: res.data });
+                    M.AutoInit();
+                    this.statefulRadioButtons();
+                    //This needs to happen in a click event later
+                    this.getChartData();
+                })
+            .catch(err => console.log(err));
+    };
+
+    componentDidUpdate() {
+        M.AutoInit();
+    }
+
+        //Post each student's data to the database
+    //Show a modal when student data has been saved and reset radio buttons
+
+    statefulRadioButtons() {
+        console.log(this.saveData.radioButtons);
+    };
+
+    // Handles updating component state when the user types into the input field
+    handleInputChange = event => {
+        //Save the value to check if it is an empty string (or, for our purposes, null)- in which case we will not update the state
+        let isItNull = event.target.value;
+        if (isItNull === '') {
+            console.log('This value is null');
+        } else {
+            //Gets the name and value of the target that changed
+            const { name, value } = event.target;
+            //Sets the state of the selected (and not-null) object in the newData object, which also updates correctly if the same behavior row's value is changed again before the save data button is clicked
+            this.setState({
+                newData: {
+                    ...this.state.newData,
+                    [name]: value
+                }
+            });
+        }
+    };
+
+//   // When the form is submitted, use the API.saveData method to save the bx data
+
+//     behaviorName: { type: String, required: true, },
+//     behaviorTracked: [{
+//       behaviorValue: { type: Number, enum: [0, 1], allowNull: true, required: false },
+//       behaviorDate: { type: Date, default: Date.now(), required: true }
+//     }],
+//     status: {type: Boolean, required: true, default: true},
+//     createdAt: { type: Date, default: Date.now() },
+//     updatedAt: { type: Date, default: Date.now() },
+//     studentName: { type: String, required: false, },
+//     student: { type: Schema.Types.ObjectId, ref: "Student", required: false},
+//     teachers: [{ type: Schema.Types.ObjectId, ref: "Teacher", required: true}]
+
+    saveData = event => {
+        event.preventDefault();
+        console.log('The current state as of the button click is: ', this.state);
+    //     const dataToSave = {
+    //         //Figure out how to save the newData from the state- will likely need a for each or map
+    //     }
+    //     API.saveData({ dataToSave })
+    //     // Then reset form
+    //     .then(res => this.loadBehaviors())
+    //     .catch(err => console.log(err));
+
+        this.setState({ newData: {}, radioButtons: {} });
     };
 
     render() {
         return (
             <div>
                 <div className="side-nav">
-                    <SideNav />
+                    <SideNav behaviorInfo={this.state.behaviorInfo}/>
                 </div>
                 <div className="main-right">
                     <TopNav />
                     <BxChart />
-                    <div className="container">
+                    <RatingForm 
+                        behaviorInfo={this.state.behaviorInfo}
+                        handleInputChange={this.handleInputChange.bind()}
+                        saveData={this.saveData.bind()}
+                        />
+                    {/* <div className="container">
                         <Switch>
                             <Route exact path={`${this.props.match.path}`} component={RatingForm} />
                             <Route exact path={`${this.props.match.path}/chart`} component={BxChart} />
                         </Switch>  
-                    </div>
+                    </div> */}
                 </div>
             </div>
         );
