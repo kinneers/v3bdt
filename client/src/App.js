3754f88d2a153244086cf370bbf9cfe0facac9f5
Jodi Bell <jodi@jodibell.com>
2019-04-24 16:07:49 -0400

    persistence working and double login gone

--- client/src/App.js
+++ client/src/App.js
@@ -7,12 +7,40 @@ import Teacher from './pages/Teacher';
 import Student from './pages/Student';
 import Admin from './pages/Admin';
 import Authenticator from './components/Auth';
+import API from './utils/API';
 
 class App extends Component {
 
     state = {
         user: null
     };
+
+    componentWillMount() {
+        const email = window.localStorage.getItem("email");
+        const accessToken = window.localStorage.getItem("accessToken");
+        const idToken = window.localStorage.getItem("idToken");
+        const refreshToken = window.localStorage.getItem("refreshToken");
+        console.log(accessToken);
+        if (email && accessToken !== "undefined" && !this.state.user) {
+            API.associateUser(email, accessToken).then(({data}) => {
+                this.setState({user: {
+                    accessToken: {jwtToken: accessToken}, idToken: {jwtToken: idToken}, refreshToken: {token: refreshToken}, ...data
+                }})
+            })
+            
+        }
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (!prevState.user) {
+            if (this.state.user && this.state.user.userName) {
+                window.localStorage.setItem("email", this.state.user.userName)
+                window.localStorage.setItem("accessToken", this.state.user.accessToken.jwtToken)
+                window.localStorage.setItem("idToken", this.state.user.idToken.jwtToken)
+                window.localStorage.setItem("refreshToken", this.state.user.refreshToken.token)
+            }
+        }
+    }
 
     handleUser = (user) => {
         this.setState({ user });
