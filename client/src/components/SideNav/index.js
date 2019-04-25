c290bfb667b3077a012ae21061aba0a7f1d7affd
lstasio5 <lisa.stasio@gmail.com>
2019-04-25 06:41:46 -0400

    fixed some css issues

--- client/src/components/SideNav/index.js
+++ client/src/components/SideNav/index.js
@@ -3,7 +3,7 @@ import './style.css';
     
 function SideNav(props) {
     return(
-        <div>
+        <div className="burger">
             <ul id="slide-out" className="sidenav sidenav-fixed">
                 <li className="top">Welcome!</li>
                     {(props.behaviorInfo.behaviors) ? (
