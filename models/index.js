474c6b645de1b294703002591c9fba5abd81efe3
Billy Sterling <44212511+BillySterling@users.noreply.github.com>
2019-05-08 17:23:08 -0400

    seeds file update for User data

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 module.exports = {
   Teacher: require("./teacher"), 
   Student: require("./student"),
+  User: require("./user"),
   Behavior: require("./behavior"),
   BehaviorData: require("./behaviorData"),
   Admin: require("./admin")
